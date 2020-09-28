c07bde023895c104267b7951e71ab65f4ce23048
Stefan Horst <stefan@ultrachaos.de>
2020-09-28 22:29:43 +0200

    fix bw 4

--- metadata.py
+++ metadata.py
@@ -1,27 +1,31 @@
-@metadata_processor
+@metadata_reactor
 def add_iptables_rules(metadata):
-    if node.has_bundle('iptables'):
-        allowed_hosts = set([])
-        for user_name, mysql_config in metadata.get('mysql', {}).get('users', {}).items():
-            allowed_hosts.update(mysql_config.get('allowed_hosts', []))
+    if not node.has_bundle('iptables'):
+        raise DoNotRunAgain
 
-        for allowed_host in sorted(allowed_hosts):
-            metadata += repo.libs.iptables.accept().chain('INPUT').source(allowed_host).tcp().dest_port(3306)
+    iptables_rules = {}
+    allowed_hosts = set([])
+    for user_name, mysql_config in metadata.get('mysql/users', {}).items():
+        allowed_hosts.update(mysql_config.get('allowed_hosts', []))
 
-    return metadata, DONE
+    for allowed_host in sorted(allowed_hosts):
+        iptables_rules += repo.libs.iptables.accept().chain('INPUT').source(allowed_host).tcp().dest_port(3306)
+
+    return iptables_rules
 
 
-@metadata_processor
+@metadata_reactor
 def add_restic_rules(metadata):
-    if node.has_bundle('restic'):
-        if 'restic' not in metadata:
-            metadata['restic'] = {}
+    if not node.has_bundle('restic'):
+        raise DoNotRunAgain
 
-        if 'stdin_commands' not in metadata['restic']:
-            metadata['restic']['stdin_commands'] = {}
+    restic_cmd = {}
+    for db in metadata.get('mysql/dbs', []):
+        restic_cmd['mysql_{}.sql'.format(db)] = \
+            'mysqldump --defaults-extra-file=/etc/mysql/debian.cnf {}'.format(db)
 
-        for db in metadata.get('mysql', {}).get('dbs', []):
-            metadata['restic']['stdin_commands']['mysql_{}.sql'.format(db)] = \
-                'mysqldump --defaults-extra-file=/etc/mysql/debian.cnf {}'.format(db)
-
-    return metadata, DONE
+    return {
+        'restic': {
+            'stdin_commands': restic_cmd,
+        }
+    }
